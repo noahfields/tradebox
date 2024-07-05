d799f5ed26685bc91f4d008c517db6a6ee15f074
Noah Fields <noahfields@noahfieldsmba.twcny.rr.com>
2024-07-05 17:19:47 -0400

    added mkdir logs in log.py

--- log.py
+++ log.py
@@ -1,7 +1,9 @@
 import datetime
+import os
 
 
 def append(message):
+    os.mkdir('logs')
     log_filename = f'logs/log-{datetime.datetime.now().strftime("%Y-%m-%d")}.txt'
     log_file = open(log_filename, "a")
     log_file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"))
