de1b211dc2ac6655513bdc68b168a4f860474df9
Noah Fields <noahfields@noahfieldsmba.twcny.rr.com>
2024-07-06 15:53:33 -0400

    added email.py

--- config-default-must-rename.py
+++ config-default-must-rename.py
@@ -5,3 +5,9 @@
 # ENTER ROBINHOOD USERNAME AND PASSWORD
 robinhood_username = ''
 robinhood_password = ''
+
+smtp_username = ''
+smtp_password = ''
+smtp_server = ''
+smtp_port = 465  # port for gmail ssl if using gmail's app smtp service
+notifcation_address = ''
