6b540fc7aa7f1552cf62b811cfeabc2e1bcdce6c
Noah Fields <noahfields@gmail.com>
2024-07-10 01:03:19 -0400

    long lines in tradebox.py

--- tradebox.py
+++ tradebox.py
@@ -11,24 +11,24 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def hello() -> str:
     return 'tradebox'
 
 
 @app.route('/orders/execute/<order_id>', methods=['POST', 'GET'])
-def execute_order(order_id):
+def execute_order(order_id: int) -> str:
     log.append(f'tradebox.py: execute_order(): executing order_id {order_id}')
     tradeapi.execute_order(order_id)
     return str(order_id)
 
 
 @app.route('/console')
-def console():
+def console() -> str:
     return 'console'
 
 
 @app.route('/pythonversion')
-def python_version():
+def python_version() -> str:
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
     python_version = sys.version
     return f'{now} <br/><br/> {python_version}'
@@ -40,7 +40,8 @@ if __name__ == '__main__':
     elif config.ENVIRONMENT == 'development':
         app.run(host='0.0.0.0', port=5555, debug=True)
     else:
-        msg = 'config.ENVIRONMENT is not set or has an invalid value. \nPlease define ENVIRONMENT=\'production\' or ENVIRONMENT=\'development\' in config.py. \n Defaulting to production mode.'
-        print(msg)
+        msg = 'config.ENVIRONMENT is not set or has an invalid value.\n' \
+            + 'Please define ENVIRONMENT=\'production\' or ENVIRONMENT=\'development\' ' \
+            + ' in config.py. \n Defaulting to production mode.'
         log.append(msg)
         app.run(host='0.0.0.0')
