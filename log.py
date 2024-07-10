7ea191be4882bb48f3f9d6a29561a449f3ce0ca4
Noah Fields <noahfields@gmail.com>
2024-07-10 02:25:23 -0400

    updated log.py to use with statement

--- log.py
+++ log.py
@@ -14,8 +14,9 @@ def append(message: str) -> None:
     log_filename = f'log-{datetime.datetime.now().strftime("%Y-%m-%d")}.txt'
     log_file_path = os.path.join(LOG_DIR, log_filename)
 
-    log_file = open(log_file_path, "a")
-    log_file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"))
-    log_file.write("\n")
-    log_file.write(message)
-    log_file.write("\n\n")
+    with open(log_file_path, mode='a', encoding='utf-8') as log_file:
+        log_file.write(datetime.datetime.now().strftime(
+            '%Y-%m-%d %H:%M:%S.%f'))
+        log_file.write('\n')
+        log_file.write(message)
+        log_file.write('\n\n')
