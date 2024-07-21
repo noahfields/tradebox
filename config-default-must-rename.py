89279a25f5eb958230398d364f0a71f9faadb382
Noah Fields <noahfields@gmail.com>
2024-07-21 15:09:02 -0400

    updated boilerplate config file

--- config-default-must-rename.py
+++ config-default-must-rename.py
@@ -1,10 +1,11 @@
 """Configuration settings for Tradebox."""
+
 # WARNING
 # YOU MUST CHANGE THE NAME OF THIS FILE to 'config.py'
 # FOR TRADEBOX TO RUN
 
-# ENVIRONMENT = 'production' or ENVIRONMENT = 'development'
-ENVIRONMENT = ''
+# URL
+TRADEBOX_APP_ADDRESS = 'http://127.0.0.1/'  # must end with '/'
 
 # ENTER ROBINHOOD USERNAME AND PASSWORD
 ROBINHOOD_USERNAME = ''
@@ -18,8 +19,11 @@ SMTP_SERVER = ''
 SMTP_PORT = 465  # port for Gmail SSL if using Gmail's SMTP servers
 NOTIFICATION_ADDRESS = ''
 
-# SERVER INFO
-TRADEBOX_APP_ADDRESS = 'http://127.0.0.1/'  # must end with '/'
+# DEBUG ENVIRONMENT SETTINGS
+DEV_IP='127.0.0.1'
+DEV_PORT=5555
+DEV_DEBUG=True
+
 # change only if needed (for example, to save database when re-cloning tradebox application)
 DATABASE_DIR = '.'
 DATABASE_NAME = 'db.sqlite3'  # change only if needed
