6f0f2fa1983d3ded6da3aeabb059bf138734ad0d
Noah Fields <noahfields@gmail.com>
2024-07-21 15:04:59 -0400

    updated tradebox.py Flask development server and formatting in log.py

--- tradebox.py
+++ tradebox.py
@@ -1,7 +1,7 @@
 """Flask server for Tradebox API."""
 
-import sys
 import datetime
+import sys
 
 from flask import Flask
 
@@ -12,38 +12,43 @@ import tradeapi
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index() -> str:
-    return "tradebox"
+    return 'tradebox'
 
 
-@app.route("/orders/execute/<order_id>", methods=["POST", "GET"])
+@app.route('/orders/execute/<order_id>', methods=['POST', 'GET'])
 def execute_order(order_id: int) -> str:
-    msg = f"tradebox.py: execute_order(): executing order_id {order_id}"
+    msg = f'tradebox.py: execute_order(): executing order_id {order_id}'
     log.append(msg)
 
     tradeapi.execute_order(order_id)
     return str(order_id)
 
 
-@app.route("/pythonversion")
+@app.route('/pythonversion')
 def python_version() -> str:
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
     python_version = sys.version
-    return f"{now} <br/><br/> {python_version}"
+    return f'{now} <br/><br/> {python_version}'
 
 
-if __name__ == "__main__":
-    if config.ENVIRONMENT == "production":
-        # Nginx/gunicorn (or other WSGI server) can use a socket file
-        # (e.g. tradebox/tradebox.socket)
-        # for WSGI interface.
-        # See README.me for a link to setup instructions.
-        app.run(host="0.0.0.0")
-    elif config.ENVIRONMENT == "development":
-        # Port 5555 to avoid conflict with MacOS port 5000.
-        # Flask default is 5000.
-        app.run(host="127.0.0.1", port=5555, debug=True)
-    else:
-        msg = "Error: config.ENVIRONMENT is not set or has an invalid value. Please define ENVIRONMENT='production' or ENVIRONMENT='development' in config.py."
-        log.append(msg)
+if __name__ == '__main__':
+    # This section runs a local development server.
+    # Do not use in production.
+    #
+    # Running 'python tradebox.py' will run a local development server: 
+    # IP Address, Port, and Debug settings in config.py.
+    #
+    # Alternatively, you can start the development server
+    # with 'flask --app tradebox run'
+    # on 127.0.0.1:5000.
+    #
+    # When running the Flask application in production
+    # via gunicorn or other wsgi server
+    # this section is not run and config.py has 
+    # no effect on server settings.
+    app.run(host=config.DEV_IP, 
+            port=config.DEV_PORT, 
+            debug=config.DEV_DEBUG
+        )
