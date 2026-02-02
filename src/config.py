f39fa7360deb6c66a57a3da148c24e6e913d3932
Noah Fields <noahfields@gmail.com>
2026-02-01 20:12:31 -0500

    fixed some config stuff

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ dotenv.load_dotenv()
 # URL
 # if in development environment, leave as is
 # if in production, please change to your full public domain name (ideally using https instead of http)
-TRADEBOX_APP_ADDRESS = 'http://127.0.0.1/'  # must end with '/'
+TRADEBOX_PUBLIC_URL = os.getenv("TRADEBOX_PUBLIC_URL")  # must end with '/'
 
 # ENTER ROBINHOOD USERNAME AND PASSWORD
 ROBINHOOD_USERNAME = os.getenv("ROBINHOOD_USERNAME")
@@ -22,22 +22,22 @@ PUSHOVER_USER_TOKEN = os.getenv("PUSHOVER_USER_TOKEN") # Pushover User Key (avai
 PUSHOVER_API_TOKEN = os.getenv("PUSHOVER_API_TOKEN") # Pushover API Token/Key (under "Your Applications", you need to set up an application for this key)
 
 # DEBUG ENVIRONMENT SETTINGS
-DEV_FLASK_IP='127.0.0.1'
-DEV_FLASK_PORT=5555
-DEV_FLASK_DEBUG=True
+DEVELOPMENT_FLASK_IP=os.getenv("DEVELOPMENT_FLASK_IP")
+DEVELOPMENT_FLASK_PORT=os.getenv("DEVELOPMENT_FLASK_PORT")
+DEVELOPMENT_FLASK_DEBUG=os.getenv("DEVELOPMENT_FLASK_DEBUG")
 
 # Refresh Intervals (seconds)
-MARKET_DATA_REFRESH_INTERVAL=os.getenv("MARKET_DATA_REFRESH_INTERVAL", 5)
-OPEN_POSITIONS_REFRESH_INTERVAL=os.getenv("OPEN_POSITIONS_REFRESH_INTERVAL", 5)
-BROKER_ORDERS_REFRESH_INTERVAL=os.getenv("BROKER_ORDERS_REFRESH_INTERVAL", 15)
-MAXIMUM_REFRESH_INTERVAL=os.getenv("MAXIMUM_REFRESH_INTERVAL", 60)
-RUNNER_FAILURE_ADJUSTMENT=os.getenv("RUNNER_FAILURE_ADJUSTMENT", 5)
-RUNNER_SUCCESS_ADJUSTMENT=os.getenv("RUNNER_SUCCESS_ADJUSTMENT", 1)
+MARKET_DATA_REFRESH_INTERVAL=os.getenv("MARKET_DATA_REFRESH_INTERVAL")
+OPEN_POSITIONS_REFRESH_INTERVAL=os.getenv("OPEN_POSITIONS_REFRESH_INTERVAL")
+BROKER_ORDERS_REFRESH_INTERVAL=os.getenv("BROKER_ORDERS_REFRESH_INTERVAL")
+MAXIMUM_REFRESH_INTERVAL=os.getenv("MAXIMUM_REFRESH_INTERVAL")
+RUNNER_FAILURE_ADJUSTMENT=os.getenv("RUNNER_FAILURE_ADJUSTMENT")
+RUNNER_SUCCESS_ADJUSTMENT=os.getenv("RUNNER_SUCCESS_ADJUSTMENT")
 
-DB_USER = os.getenv("DB_USER", "tradebox")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "devonly")
-DB_NAME = os.getenv("DB_NAME", "tradebox")
-DB_PORT = os.getenv("DB_PORT", "5432")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD"))
+DB_NAME = os.getenv("DB_NAME")
+DB_PORT = os.getenv("DB_PORT")
 DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost:{DB_PORT}/{DB_NAME}"
 
 FILE_LOGGING = bool(os.getenv("FILE_LOGGING"))
@@ -48,4 +48,5 @@ LOG_LEVEL = os.getenv("LOG_LEVEL")
 SRC_DIR = pathlib.Path(__file__).parent.absolute().as_posix()
 REPO_DIR = pathlib.Path(__file__).parent.parent.absolute().as_posix()
 LOG_BASE_DIR = os.path.join(REPO_DIR, "logs")
+LOG_RUNNERS_DIR = os.path.join(LOG_BASE_DIR, "runners")
 LOG_ORDERS_DIR = os.path.join(LOG_BASE_DIR, "orders")
