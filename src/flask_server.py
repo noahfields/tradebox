d36edafd3650cee0de6a635fc2c25427d3adce5f
Noah Fields <noahfields@gmail.com>
2026-02-02 02:20:11 -0500

    changed flask name

---
+++ src/flask_server.py
@@ -0,0 +1,69 @@
+"""Flask server for Tradebox API."""
+
+import datetime
+import sys
+
+from flask import Flask
+
+import config
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index() -> str:
+    current_datetime_string = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    python_version = sys.version
+
+    html = 'Welcome to Tradebox. <br />' \
+         + f'Current date and time: {current_datetime_string} <br />' \
+         + f'Python version: {python_version}'
+    return(html)
+
+
+# @app.route('/orders/execute/<order_uuid>', methods=['POST', 'GET'])
+# def execute_order(order_id: int) -> str:
+#     try:
+#         order_id = int(order_id)
+#     except ValueError:
+#         msg = f'tradebox.execute_order({order_id}). order_id: "{order_id}" is not a valid integer.'
+#         log.append(msg)
+
+#         html = 'error'
+#         return html
+
+#     try:
+#         msg = f'tradebox.py: execute_order(): executing order_id {order_id}. \n' \
+#             + f'Entering tradeapi.execute_order({order_id}).'
+#         log.append(msg)
+
+#         tradeapi.execute_order(order_id)
+
+#         html = f'Executed order #{order_id}.'
+#         return html
+#     except Exception as ex:
+#         log_traceback(ex)
+#         html = f'There was an issue executing order #{order_id}. Writing traceback to log file.'
+#         return html
+
+
+if __name__ == '__main__':
+    # This section runs a local development server.
+    # Do not use in production.
+    #
+    # Running 'python tradebox.py' will run a local development server: 
+    # IP Address, Port, and Debug settings in config.py.
+    #
+    # Alternatively, you can start the development server
+    # with 'flask --app tradebox run'
+    # on 127.0.0.1:5000.
+    #
+    # When running the Flask application in production
+    # via gunicorn or other wsgi server
+    # this section is not run and config.py has 
+    # no effect on server settings.
+    app.run(
+            host=config.DEVELOPMENT_FLASK_IP, 
+            port=int(config.DEVELOPMENT_FLASK_PORT),
+            debug=bool(config.DEVELOPMENT_FLASK_DEBUG)
+    )
--- src/wsgi.py
+++ src/wsgi.py
@@ -1,6 +1,6 @@
 """WSGI connection for Tradebox Flask server."""
 
-from order_trigger_server import app
+from flask_server import app
 
 if __name__ == "__main__":
     app.run()
